2ab7b754776c39adeaf6cbd5a907123abb5a868d
ecklf <ecklf@icloud.com>
2021-11-18 08:36:10 +0100

    fix: options

--- src/index.ts
+++ src/index.ts
@@ -6,18 +6,21 @@ const dataAttributes = {
 };
 
 export = plugin.withOptions((options) => ({ addUtilities, addVariant, e }) => {
-  const variantPrefix = options?.variantPrefix ?? "radix";
+  const variantPrefix = options
+    ? options.variantPrefix === ""
+      ? ""
+      : `${options.variantPrefix}-`
+    : "radix-";
+
+  addUtilities({
+    [`.origin-${variantPrefix}dropdown`]: {
+      "transform-origin": "var(--radix-dropdown-menu-content-transform-origin)",
+    },
+  });
 
   Object.keys(dataAttributes).forEach((k) => {
     dataAttributes[k as keyof typeof dataAttributes].forEach((v) => {
-      addUtilities({
-        [`.origin-${variantPrefix ? `${variantPrefix}-` : ""}dropdown`]: {
-          "transform-origin":
-            "var(--radix-dropdown-menu-content-transform-origin)",
-        },
-      });
-
-      let variantName = `${variantPrefix ? `${variantPrefix}-` : ""}${k}-${v}`;
+      let variantName = `${variantPrefix}${k}-${v}`;
       let selector = `data-${k}="${v}"`;
 
       addVariant(`${variantName}`, ({ modifySelectors, separator }) => {
