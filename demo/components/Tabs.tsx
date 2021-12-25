67b177e18ae33501622b29f33c0b394adf77e448
ecklf <ecklf@icloud.com>
2021-12-25 23:02:16 +0100

    feat(demo): add tabs

---
+++ demo/components/Tabs.tsx
@@ -0,0 +1,73 @@
+import * as TabsPrimitive from "@radix-ui/react-tabs";
+import cx from "classnames";
+import React from "react";
+
+interface Tab {
+  title: string;
+  value: string;
+}
+
+const tabs: Tab[] = [
+  {
+    title: "Inbox",
+    value: "tab1",
+  },
+  {
+    title: "Today",
+    value: "tab2",
+  },
+
+  {
+    title: "Upcoming",
+    value: "tab3",
+  },
+];
+
+interface Props {}
+
+const Tabs = (props: Props) => {
+  return (
+    <TabsPrimitive.Root defaultValue="tab1">
+      <TabsPrimitive.List
+        className={cx("flex w-full rounded-t-lg dark:bg-gray-800")}
+      >
+        {tabs.map(({ title, value }) => (
+          <TabsPrimitive.Trigger
+            key={`tab-trigger-${value}`}
+            value={value}
+            className={cx(
+              "first:rounded-tl-lg last:rounded-tr-lg",
+              "border-b first:border-r last:border-l border-gray-600",
+              "radix-state-active:border-b-purple-700",
+              "flex-1 px-3 py-2.5",
+              "focus:outline-none focus:z-10 focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75"
+            )}
+          >
+            <span className="text-sm font-medium dark:text-gray-300">
+              {title}
+            </span>
+          </TabsPrimitive.Trigger>
+        ))}
+      </TabsPrimitive.List>
+      {tabs.map(({ value }) => (
+        <TabsPrimitive.Content
+          key={`tab-content-${value}`}
+          value={value}
+          className={cx("px-6 py-4 rounded-b-lg dark:bg-gray-800")}
+        >
+          <span className="text-sm dark:text-gray-300">
+            {
+              {
+                tab1: "Your inbox is empty",
+                tab2: "Make some coffee",
+                tab3: "Order more coffee",
+              }[value]
+            }
+          </span>
+        </TabsPrimitive.Content>
+      ))}
+    </TabsPrimitive.Root>
+  );
+};
+
+export default Tabs;
