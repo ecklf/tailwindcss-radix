1ea8a5cbbe0317da57a8e12c9bb9eb8eddeea2ca
ecklf <ecklf@icloud.com>
2021-12-25 10:20:11 +0100

    feat(demo): use hooks for toggle state

--- demo/components/Toggle.tsx
+++ demo/components/Toggle.tsx
@@ -1,27 +1,26 @@
 import { StarFilledIcon, StarIcon } from "@radix-ui/react-icons";
 import * as TogglePrimitive from "@radix-ui/react-toggle";
 import cx from "classnames";
-import React from "react";
+import React, { useState } from "react";
 
 interface Props {}
 
 const Toggle = (props: Props) => {
+  const [starred, setStarred] = useState(false);
+
   return (
     <TogglePrimitive.Root
+      defaultPressed={starred}
+      onPressedChange={setStarred}
       className={cx(
-        "group",
         "radix-state-on:bg-gray-900",
         "radix-state-off:bg-gray-200 dark:radix-state-off:bg-gray-800",
         "relative inline-flex justify-center items-center px-4 py-2 text-sm font-medium text-gray-300 bg-gray-800 rounded-md select-none",
         "focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75"
       )}
     >
-      <StarIcon className="group-radix-state-on:hidden" />
-      <StarFilledIcon className="text-yellow-400 group-radix-state-off:hidden" />
-      <span className="ml-1.5 leading-5 group-radix-state-on:hidden">Star</span>
-      <span className="ml-1.5 leading-5 group-radix-state-off:hidden">
-        Starred
-      </span>
+      {starred ? <StarFilledIcon className="text-yellow-400" /> : <StarIcon />}
+      <span className="ml-2 leading-5">{starred ? "Starred" : "Star"}</span>
     </TogglePrimitive.Root>
   );
 };
