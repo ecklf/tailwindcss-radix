b1df53421334b1dff68705e3287b32d90ea01992
ecklf <ecklf@icloud.com>
2021-12-25 20:08:53 +0100

    feat(demo): add toolbar

---
+++ demo/components/Toolbar.tsx
@@ -0,0 +1,119 @@
+import {
+  FontBoldIcon,
+  FontItalicIcon,
+  TextAlignCenterIcon,
+  TextAlignLeftIcon,
+  TextAlignRightIcon,
+  UnderlineIcon,
+} from "@radix-ui/react-icons";
+import * as ToolbarPrimitive from "@radix-ui/react-toolbar";
+import cx from "classnames";
+import React, { ReactElement } from "react";
+
+interface ToggleItem {
+  value: string;
+  label: string;
+  icon: ReactElement;
+}
+
+const fontSettings: ToggleItem[] = [
+  {
+    value: "bold",
+    label: "Font bold",
+    icon: <FontBoldIcon />,
+  },
+  {
+    value: "italic",
+    label: "Font italic",
+    icon: <FontItalicIcon />,
+  },
+  {
+    value: "underline",
+    label: "Underline",
+    icon: <UnderlineIcon />,
+  },
+];
+
+const textSettings: ToggleItem[] = [
+  {
+    value: "left",
+    label: "Text left",
+    icon: <TextAlignLeftIcon />,
+  },
+  {
+    value: "center",
+    label: "Text center",
+    icon: <TextAlignCenterIcon />,
+  },
+  {
+    value: "right",
+    label: "Text right",
+    icon: <TextAlignRightIcon />,
+  },
+];
+
+interface Props {}
+
+const Toolbar = (props: Props) => {
+  return (
+    <ToolbarPrimitive.Root className="flex px-2.5 py-2 space-x-4 bg-gray-800 rounded-lg">
+      <ToolbarPrimitive.ToggleGroup type="multiple" aria-label="Font settings">
+        {fontSettings.map(({ value, label, icon }, i) => (
+          <ToolbarPrimitive.ToggleItem
+            key={`group-item-${value}-${label}`}
+            value={value}
+            aria-label={label}
+            className={cx(
+              "radix-state-on:dark:bg-gray-900",
+              "px-2.5 py-2 dark:bg-gray-800",
+              "first:rounded-l-md last:rounded-r-md",
+              "border-y first:border-x last:border-x border-gray-700 radix-state-on:border-transparent",
+              "focus:relative focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75 focus-visible:z-20"
+            )}
+          >
+            {React.cloneElement(icon, {
+              className: "w-5 h-5 dark:text-gray-200",
+            })}
+          </ToolbarPrimitive.ToggleItem>
+        ))}
+      </ToolbarPrimitive.ToggleGroup>
+
+      <ToolbarPrimitive.ToggleGroup
+        type="single"
+        defaultValue="center"
+        aria-label="Text settings"
+      >
+        {textSettings.map(({ value, label, icon }, i) => (
+          <ToolbarPrimitive.ToggleItem
+            key={`group-item-${value}-${label}`}
+            value={value}
+            aria-label={label}
+            className={cx(
+              "radix-state-on:dark:bg-gray-900",
+              "px-2.5 py-2 dark:bg-gray-800",
+              "first:rounded-l-md last:rounded-r-md",
+              "border-y first:border-x last:border-x border-gray-700 radix-state-on:border-transparent",
+              "focus:relative focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75 focus-visible:z-20"
+            )}
+          >
+            {React.cloneElement(icon, {
+              className: "w-5 h-5 dark:text-gray-200",
+            })}
+          </ToolbarPrimitive.ToggleItem>
+        ))}
+      </ToolbarPrimitive.ToggleGroup>
+
+      <ToolbarPrimitive.Separator className="w-px h-auto mx-4 my-0.5 bg-gray-700" />
+
+      <ToolbarPrimitive.Link
+        className="inline-flex self-center pr-2.5 text-sm text-gray-500 dark:text-gray-400"
+        href="https://github.com/ecklf/tailwindcss-radix"
+        target="_blank"
+      >
+        Edited 2 hours ago
+      </ToolbarPrimitive.Link>
+    </ToolbarPrimitive.Root>
+  );
+};
+
+export default Toolbar;
--- demo/pages/index.tsx
+++ demo/pages/index.tsx
@@ -8,6 +8,7 @@ import Slider from "../components/Slider";
 import Switch from "../components/Switch";
 import Toggle from "../components/Toggle";
 import ToggleGroup from "../components/ToggleGroup";
+import Toolbar from "../components/Toolbar";
 
 interface Props {}
 
@@ -140,6 +141,15 @@ const Demo = (props: Props) => {
             <div className="absolute inset-0 rounded-xl bg-gradient-to-br from-pink-600 via-fuchsia-800 to-purple-600 dark:from-pink-800 dark:via-fuchsia-900 dark:to-purple-800" />
             <div className="z-10">
               <ToggleGroup />
+            </div>
+          </div>
+        </div>
+
+        <div className="w-full h-[500px]">
+          <div className="relative flex items-center justify-center w-full h-full px-6 overflow-hidden shadow-md rounded-xl">
+            <div className="absolute inset-0 rounded-xl bg-gradient-to-br from-pink-600 via-fuchsia-800 to-purple-600 dark:from-pink-800 dark:via-fuchsia-900 dark:to-purple-800" />
+            <div className="z-10">
+              <Toolbar />
             </div>
           </div>
         </div>
