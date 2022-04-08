6387acf63df25a4053b8b986966883e97dad1774
ecklf <ecklf@icloud.com>
2022-04-08 21:37:28 +0200

    style(demo): update anims

--- demo/tailwind.config.js
+++ demo/tailwind.config.js
@@ -63,32 +63,32 @@ module.exports = {
           "100%": { transform: "translateX(0)", opacity: 1 },
         },
         "enter-from-left": {
-          from: { transform: "translateX(-200px)", opacity: 0 },
-          to: { transform: "translateX(0)", opacity: 1 },
+          "0%": { transform: "translateX(-200px)", opacity: 0 },
+          "100%": { transform: "translateX(0)", opacity: 1 },
         },
         "exit-to-right": {
-          from: { transform: "translateX(0)", opacity: 1 },
-          to: { transform: "translateX(200px)", opacity: 0 },
+          "0%": { transform: "translateX(0)", opacity: 1 },
+          "100%": { transform: "translateX(200px)", opacity: 0 },
         },
         "exit-to-left": {
-          from: { transform: "translateX(0)", opacity: 1 },
-          to: { transform: "translateX(-200px)", opacity: 0 },
+          "0%": { transform: "translateX(0)", opacity: 1 },
+          "100%": { transform: "translateX(-200px)", opacity: 0 },
         },
         "scale-in-content": {
-          from: { transform: "rotateX(-30deg) scale(0.9)", opacity: 0 },
-          to: { transform: "rotateX(0deg) scale(1)", opacity: 1 },
+          "0%": { transform: "rotateX(-30deg) scale(0.9)", opacity: 0 },
+          "100%": { transform: "rotateX(0deg) scale(1)", opacity: 1 },
         },
         "scale-out-content": {
-          from: { transform: "rotateX(0deg) scale(1)", opacity: 1 },
-          to: { transform: "rotateX(-10deg) scale(0.95)", opacity: 0 },
+          "0%": { transform: "rotateX(0deg) scale(1)", opacity: 1 },
+          "100%": { transform: "rotateX(-10deg) scale(0.95)", opacity: 0 },
         },
         "fade-in": {
-          from: { opacity: 0 },
-          to: { opacity: 1 },
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
         },
         "fade-out": {
-          from: { opacity: 1 },
-          to: { opacity: 0 },
+          "0%": { opacity: 1 },
+          "100%": { opacity: 0 },
         },
       },
       animation: {
