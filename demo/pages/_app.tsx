9c62e0a57e9b34620f001fbc22168df343293260
ecklf <ecklf@icloud.com>
2022-11-11 15:51:12 +0100

    add vercel analytics

--- demo/pages/_app.tsx
+++ demo/pages/_app.tsx
@@ -1,7 +1,13 @@
 import "../css/tailwind.css";
+import { Analytics } from "@vercel/analytics/react";
 
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default App;
