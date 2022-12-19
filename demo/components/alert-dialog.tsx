44c9793dc81e475f95cb57fad7e36584c2098ad8
ecklf <ecklf@icloud.com>
2022-12-19 09:54:26 +0100

    fix(demo): add portal to dialogs

--- demo/components/alert-dialog.tsx
+++ demo/components/alert-dialog.tsx
@@ -14,72 +14,74 @@ const AlertDialog = (props: Props) => {
       <AlertDialogPrimitive.Trigger asChild>
         <Button>Click</Button>
       </AlertDialogPrimitive.Trigger>
-      <Transition.Root show={isOpen}>
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <AlertDialogPrimitive.Overlay
-            forceMount
-            className="fixed inset-0 z-20 bg-black/50"
-          />
-        </Transition.Child>
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0 scale-95"
-          enterTo="opacity-100 scale-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100 scale-100"
-          leaveTo="opacity-0 scale-95"
-        >
-          <AlertDialogPrimitive.Content
-            forceMount
-            className={cx(
-              "fixed z-50",
-              "w-[95vw] max-w-md rounded-lg p-4 md:w-full",
-              "top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]",
-              "bg-white dark:bg-gray-800",
-              "focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75"
-            )}
+      <AlertDialogPrimitive.Portal forceMount>
+        <Transition.Root show={isOpen}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
           >
-            <AlertDialogPrimitive.Title className="text-sm font-medium text-gray-900 dark:text-gray-100">
-              Are you absolutely sure?
-            </AlertDialogPrimitive.Title>
-            <AlertDialogPrimitive.Description className="mt-2 text-sm font-normal text-gray-700 dark:text-gray-400">
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
-            </AlertDialogPrimitive.Description>
-            <div className="mt-4 flex justify-end space-x-2">
-              <AlertDialogPrimitive.Cancel
-                className={cx(
-                  "inline-flex select-none justify-center rounded-md px-4 py-2 text-sm font-medium",
-                  "bg-white text-gray-900 hover:bg-gray-50 dark:bg-gray-700 dark:text-gray-100 hover:dark:bg-gray-600",
-                  "border border-gray-300 dark:border-transparent",
-                  "focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75"
-                )}
-              >
-                Cancel
-              </AlertDialogPrimitive.Cancel>
-              <AlertDialogPrimitive.Action
-                className={cx(
-                  "inline-flex select-none justify-center rounded-md px-4 py-2 text-sm font-medium",
-                  "bg-purple-600 text-white hover:bg-purple-700 dark:bg-purple-700 dark:text-gray-100 dark:hover:bg-purple-600",
-                  "border border-transparent",
-                  "focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75"
-                )}
-              >
-                Confirm
-              </AlertDialogPrimitive.Action>
-            </div>
-          </AlertDialogPrimitive.Content>
-        </Transition.Child>
-      </Transition.Root>
+            <AlertDialogPrimitive.Overlay
+              forceMount
+              className="fixed inset-0 z-20 bg-black/50"
+            />
+          </Transition.Child>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <AlertDialogPrimitive.Content
+              forceMount
+              className={cx(
+                "fixed z-50",
+                "w-[95vw] max-w-md rounded-lg p-4 md:w-full",
+                "top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]",
+                "bg-white dark:bg-gray-800",
+                "focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75"
+              )}
+            >
+              <AlertDialogPrimitive.Title className="text-sm font-medium text-gray-900 dark:text-gray-100">
+                Are you absolutely sure?
+              </AlertDialogPrimitive.Title>
+              <AlertDialogPrimitive.Description className="mt-2 text-sm font-normal text-gray-700 dark:text-gray-400">
+                This action cannot be undone. This will permanently delete your
+                account and remove your data from our servers.
+              </AlertDialogPrimitive.Description>
+              <div className="mt-4 flex justify-end space-x-2">
+                <AlertDialogPrimitive.Cancel
+                  className={cx(
+                    "inline-flex select-none justify-center rounded-md px-4 py-2 text-sm font-medium",
+                    "bg-white text-gray-900 hover:bg-gray-50 dark:bg-gray-700 dark:text-gray-100 hover:dark:bg-gray-600",
+                    "border border-gray-300 dark:border-transparent",
+                    "focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75"
+                  )}
+                >
+                  Cancel
+                </AlertDialogPrimitive.Cancel>
+                <AlertDialogPrimitive.Action
+                  className={cx(
+                    "inline-flex select-none justify-center rounded-md px-4 py-2 text-sm font-medium",
+                    "bg-purple-600 text-white hover:bg-purple-700 dark:bg-purple-700 dark:text-gray-100 dark:hover:bg-purple-600",
+                    "border border-transparent",
+                    "focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75"
+                  )}
+                >
+                  Confirm
+                </AlertDialogPrimitive.Action>
+              </div>
+            </AlertDialogPrimitive.Content>
+          </Transition.Child>
+        </Transition.Root>
+      </AlertDialogPrimitive.Portal>
     </AlertDialogPrimitive.Root>
   );
 };
--- demo/components/dialog.tsx
+++ demo/components/dialog.tsx
@@ -15,115 +15,117 @@ const Dialog = (props: Props) => {
       <DialogPrimitive.Trigger asChild>
         <Button>Click</Button>
       </DialogPrimitive.Trigger>
-      <Transition.Root show={isOpen}>
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <DialogPrimitive.Overlay
-            forceMount
-            className="fixed inset-0 z-20 bg-black/50"
-          />
-        </Transition.Child>
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0 scale-95"
-          enterTo="opacity-100 scale-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100 scale-100"
-          leaveTo="opacity-0 scale-95"
-        >
-          <DialogPrimitive.Content
-            forceMount
-            className={cx(
-              "fixed z-50",
-              "w-[95vw] max-w-md rounded-lg p-4 md:w-full",
-              "top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]",
-              "bg-white dark:bg-gray-800",
-              "focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75"
-            )}
+      <DialogPrimitive.Portal forceMount>
+        <Transition.Root show={isOpen}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
           >
-            <DialogPrimitive.Title className="text-sm font-medium text-gray-900 dark:text-gray-100">
-              Edit profile
-            </DialogPrimitive.Title>
-            <DialogPrimitive.Description className="mt-2 text-sm font-normal text-gray-700 dark:text-gray-400">
-              Make changes to your profile here. Click save when you&apos;re
-              done.
-            </DialogPrimitive.Description>
-            <form className="mt-2 space-y-2">
-              <fieldset>
-                {/* <legend>Choose your favorite monster</legend> */}
-                <label
-                  htmlFor="firstName"
-                  className="text-xs font-medium text-gray-700 dark:text-gray-400"
-                >
-                  First Name
-                </label>
-                <input
-                  id="firstName"
-                  type="text"
-                  placeholder="Tim"
-                  autoComplete="given-name"
-                  className={cx(
-                    "mt-1 block w-full rounded-md",
-                    "text-sm text-gray-700 placeholder:text-gray-500 dark:text-gray-400 dark:placeholder:text-gray-600",
-                    "border border-gray-400 focus-visible:border-transparent dark:border-gray-700 dark:bg-gray-800",
-                    "focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75"
-                  )}
-                />
-              </fieldset>
-              <fieldset>
-                <label
-                  htmlFor="familyName"
-                  className="text-xs font-medium text-gray-700 dark:text-gray-400"
-                >
-                  Family Name
-                </label>
-                <input
-                  id="familyName"
-                  type="text"
-                  placeholder="Cook"
-                  autoComplete="family-name"
-                  className={cx(
-                    "mt-1 block w-full rounded-md",
-                    "text-sm text-gray-700 placeholder:text-gray-500 dark:text-gray-400 dark:placeholder:text-gray-600",
-                    "border border-gray-400 focus-visible:border-transparent dark:border-gray-700 dark:bg-gray-800",
-                    "focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75"
-                  )}
-                />
-              </fieldset>
-            </form>
-
-            <div className="mt-4 flex justify-end">
-              <DialogPrimitive.Close
-                className={cx(
-                  "inline-flex select-none justify-center rounded-md px-4 py-2 text-sm font-medium",
-                  "bg-purple-600 text-white hover:bg-purple-700 dark:bg-purple-700 dark:text-gray-100 dark:hover:bg-purple-600",
-                  "border border-transparent",
-                  "focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75"
-                )}
-              >
-                Save
-              </DialogPrimitive.Close>
-            </div>
-
-            <DialogPrimitive.Close
+            <DialogPrimitive.Overlay
+              forceMount
+              className="fixed inset-0 z-20 bg-black/50"
+            />
+          </Transition.Child>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <DialogPrimitive.Content
+              forceMount
               className={cx(
-                "absolute top-3.5 right-3.5 inline-flex items-center justify-center rounded-full p-1",
+                "fixed z-50",
+                "w-[95vw] max-w-md rounded-lg p-4 md:w-full",
+                "top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]",
+                "bg-white dark:bg-gray-800",
                 "focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75"
               )}
             >
-              <Cross1Icon className="h-4 w-4 text-gray-500 hover:text-gray-700 dark:text-gray-500 dark:hover:text-gray-400" />
-            </DialogPrimitive.Close>
-          </DialogPrimitive.Content>
-        </Transition.Child>
-      </Transition.Root>
+              <DialogPrimitive.Title className="text-sm font-medium text-gray-900 dark:text-gray-100">
+                Edit profile
+              </DialogPrimitive.Title>
+              <DialogPrimitive.Description className="mt-2 text-sm font-normal text-gray-700 dark:text-gray-400">
+                Make changes to your profile here. Click save when you&apos;re
+                done.
+              </DialogPrimitive.Description>
+              <form className="mt-2 space-y-2">
+                <fieldset>
+                  {/* <legend>Choose your favorite monster</legend> */}
+                  <label
+                    htmlFor="firstName"
+                    className="text-xs font-medium text-gray-700 dark:text-gray-400"
+                  >
+                    First Name
+                  </label>
+                  <input
+                    id="firstName"
+                    type="text"
+                    placeholder="Tim"
+                    autoComplete="given-name"
+                    className={cx(
+                      "mt-1 block w-full rounded-md",
+                      "text-sm text-gray-700 placeholder:text-gray-500 dark:text-gray-400 dark:placeholder:text-gray-600",
+                      "border border-gray-400 focus-visible:border-transparent dark:border-gray-700 dark:bg-gray-800",
+                      "focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75"
+                    )}
+                  />
+                </fieldset>
+                <fieldset>
+                  <label
+                    htmlFor="familyName"
+                    className="text-xs font-medium text-gray-700 dark:text-gray-400"
+                  >
+                    Family Name
+                  </label>
+                  <input
+                    id="familyName"
+                    type="text"
+                    placeholder="Cook"
+                    autoComplete="family-name"
+                    className={cx(
+                      "mt-1 block w-full rounded-md",
+                      "text-sm text-gray-700 placeholder:text-gray-500 dark:text-gray-400 dark:placeholder:text-gray-600",
+                      "border border-gray-400 focus-visible:border-transparent dark:border-gray-700 dark:bg-gray-800",
+                      "focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75"
+                    )}
+                  />
+                </fieldset>
+              </form>
+
+              <div className="mt-4 flex justify-end">
+                <DialogPrimitive.Close
+                  className={cx(
+                    "inline-flex select-none justify-center rounded-md px-4 py-2 text-sm font-medium",
+                    "bg-purple-600 text-white hover:bg-purple-700 dark:bg-purple-700 dark:text-gray-100 dark:hover:bg-purple-600",
+                    "border border-transparent",
+                    "focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75"
+                  )}
+                >
+                  Save
+                </DialogPrimitive.Close>
+              </div>
+
+              <DialogPrimitive.Close
+                className={cx(
+                  "absolute top-3.5 right-3.5 inline-flex items-center justify-center rounded-full p-1",
+                  "focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75"
+                )}
+              >
+                <Cross1Icon className="h-4 w-4 text-gray-500 hover:text-gray-700 dark:text-gray-500 dark:hover:text-gray-400" />
+              </DialogPrimitive.Close>
+            </DialogPrimitive.Content>
+          </Transition.Child>
+        </Transition.Root>
+      </DialogPrimitive.Portal>
     </DialogPrimitive.Root>
   );
 };
