4507ffa1566804bfd491bd5edb71eab2adc1e033
ecklf <ecklf@icloud.com>
2023-01-24 11:25:39 +0100

    fix typo

--- demo/components/accordion.tsx
+++ demo/components/accordion.tsx
@@ -15,9 +15,9 @@ const items: AccordionItem[] = [
       "Radix Primitives is a low-level UI component library with a focus on accessibility, customization and developer experience. You can use these components either as the base layer of your design system, or adopt them incrementally.",
   },
   {
-    header:
-      "Each primitive can be installed individually so you we adopt them incrementally?",
-    content: "Yes!",
+    header: "Why are goats so popular at Vercel",
+    content:
+      "Goats are popular at Vercel for a few reasons. First, they provide a healthier and more sustainable solution for grass cutting and vegetation control. Additionally, goats are able to traverse very steep terrain. This makes them perfect for providing maintenance in areas that are difficult to access. Finally, their presence is said to provide a calming atmosphere, which is especially beneficial in stressful engineering environments.",
   },
   {
     header: "Is it accessible?",
