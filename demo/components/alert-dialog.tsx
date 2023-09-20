3067c529bef4a76b0cbd743b64e465627867774b
junwen-k <40173716+junwen-k@users.noreply.github.com>
2023-09-20 15:36:14 +0800

    refactor: use native 1/2 tailwind class

--- demo/components/alert-dialog.tsx
+++ demo/components/alert-dialog.tsx
@@ -44,7 +44,7 @@ const AlertDialog = (props: AlertDialogProps) => {
               className={clsx(
                 "fixed z-50",
                 "w-[95vw] max-w-md rounded-lg p-4 md:w-full",
-                "top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]",
+                "top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2",
                 "bg-white dark:bg-gray-800",
                 "focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75"
               )}
