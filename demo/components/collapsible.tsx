1060113fd79be34b96b6c528e06c1a95edd6fa3d
Florentin Eckl <ecklf@icloud.com>
2025-04-07 21:14:05 +0200

    animate collapsible

--- demo/components/collapsible.tsx
+++ demo/components/collapsible.tsx
@@ -18,25 +18,32 @@ const Collapsible = () => {
         <div>My Playlists</div>
         <TriangleRightIcon className="transform duration-300 ease-in-out group-radix-state-open:rotate-90" />
       </CollapsiblePrimitive.Trigger>
-      <CollapsiblePrimitive.Content className="mt-4 flex flex-col space-y-4">
-        {["80s Synthwave", "Maximale Konzentration", "高人氣金曲"].map(
-          (title) => (
-            <div
-              key={`collapsible-${title}`}
-              className={clsx(
-                "group",
-                "ml-12 flex select-none items-center justify-between rounded-md px-4 py-2 text-left text-sm font-medium",
-                "bg-white text-gray-900 hover:bg-gray-50 dark:bg-gray-800 dark:text-gray-100 dark:hover:bg-gray-900"
-              )}
-            >
-              {title}
-              <div className="hidden items-center space-x-3 group-hover:flex">
-                <Share2Icon className="cursor-pointer text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200" />
-                <PlayIcon className="cursor-pointer text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200" />
-              </div>
-            </div>
-          )
+      <CollapsiblePrimitive.Content
+        className={clsx(
+          "overflow-hidden",
+          "radix-state-open:animate-collapsible-slide-down radix-state-closed:animate-collapsible-slide-up"
         )}
+      >
+        <div className="mt-4 flex flex-col space-y-4">
+          {["80s Synthwave", "Maximale Konzentration", "高人氣金曲"].map(
+            (title) => (
+              <div
+                key={`collapsible-${title}`}
+                className={clsx(
+                  "group",
+                  "ml-12 flex select-none items-center justify-between rounded-md px-4 py-2 text-left text-sm font-medium",
+                  "bg-white text-gray-900 hover:bg-gray-50 dark:bg-gray-800 dark:text-gray-100 dark:hover:bg-gray-900"
+                )}
+              >
+                {title}
+                <div className="hidden items-center space-x-3 group-hover:flex">
+                  <Share2Icon className="cursor-pointer text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200" />
+                  <PlayIcon className="cursor-pointer text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200" />
+                </div>
+              </div>
+            )
+          )}
+        </div>
       </CollapsiblePrimitive.Content>
     </CollapsiblePrimitive.Root>
   );
